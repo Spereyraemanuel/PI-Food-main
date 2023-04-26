a289873d6a9cc108816a19e6746fd19d49a2f821
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-04-26 12:04:06 -0300

    actualizado

--- api/src/controllers/recipesId.js
+++ api/src/controllers/recipesId.js
@@ -1,22 +1,11 @@
 require('dotenv').config();
-const { Router } = require('express')
+const recipeId = require('express').Router()
 const axios = require('axios')
 const {
  API_KEY
 } = process.env;
 
-const url = `https://api.spoonacular.com/recipes/{id}/information`
-const { Recipe, TypeDiet } = require("../models/Recipe");
 
- recipeId.get("/recipe/:id",async (res,req) => {
-try {
-  let filterRecipe;
-  //si le id pertenece a la bd busca y trae la informacion de la base de datos
- 
-}catch(error){
-
-}
- });
 
 // const getDBInfo = async () => {
 // return await Recipe.findAll({
@@ -30,12 +19,6 @@ try {
 // })
 // }
 
-// const getAllRecipes = async () => {
-//   const apiInfo = await getApiInfo()
-//   const dbInfo = await getDBInfo()
-//   const allRecipes = [...apiInfo,...dbInfo]
-//   return allRecipes;
-// }
 
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,8 @@
 const { Router } = require("express");
 const router = Router();
-const { recipeid } = require("../controllers/recipesId.js");
 
-router.use("/recipe/:id", recipeid)
-router.use("/recipes/name", recipes)
-router.use("/", diets)
+
+
   
   module.exports = router;
 
