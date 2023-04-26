60f190918f5bb45ff666c31ee29fa609cc890681
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-04-26 16:44:39 -0300

    actualizado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,15 +1,20 @@
 const { Router } = require("express")
 const router = Router()
-
-const recipes = require ("./recipesRouter")
-const types = require("./typeRouter")
-const recipe = require("./recipeRouter")
+const recipesRouter = require("./recipesRouter")
 
 
+router.use("/", recipesRouter)
 
-router.use("/recipes", recipes);
-router.use("/recipe",recipe);
-router.use("/types", types);
+
+// const recipes = require ("./recipesRouter")
+// const types = require("./typeRouter")
+// const recipe = require("./recipeRouter")
+
+
+
+// router.use("/recipes", recipes);
+// router.use("/recipe",recipe);
+// router.use("/types", types);
 
   
   module.exports = router;
--- api/src/routes/typeRouter.js
+++ api/src/routes/typeRouter.js
@@ -1,17 +1,17 @@
-const { Router } = require('express');
-const router = Router();
-const {TypeDiet} = require('../db');
-const {diets} = require('../controllers/dietsController')
+// const { Router } = require('express');
+// const router = Router();
+// const {TypeDiet} = require('../db');
+// const {diets} = require('../controllers/dietsController')
 
-router.get('/', async (req,res) => {
-        diets.forEach(e => {
-            TypeDiet.findOrCreate({
-                where: {name:e.name}
-            })
-        })
+// router.get('/', async (req,res) => {
+//         diets.forEach(e => {
+//             TypeDiet.findOrCreate({
+//                 where: {name:e.name}
+//             })
+//         })
 
-         const allTheTypes = await TypeDiet.findAll();
-        res.send(allTheTypes.map(e => e.name))
-})
+//          const allTheTypes = await TypeDiet.findAll();
+//         res.send(allTheTypes.map(e => e.name))
+// })
 
-module.exports= router;
+// module.exports= router;
