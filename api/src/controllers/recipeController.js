7c683e49f3d7cc4878e0f62e91f0c66c8a3969ff
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-04-26 13:29:25 -0300

    actualizado

--- api/src/controllers/recipeController.js
+++ api/src/controllers/recipeController.js
@@ -1,23 +1,4 @@
-const axios = require("axios");
-const Recipe = require("../models/Recipe")
 
-async function gedRecipeData(API_KEY, id){
-    try {
-        const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?api_key=${API_KEY}`);
-        const dataRecipe = response.data.map((rec)=>{
-        return {
-            id: rec.id,
-            title: rec.title,
-            image: rec.image,
-            summary: rec.summary,
-            healtScore: rec.healtScore,
-            instruction: rec.instruction,
-        }});
-    return dataRecipe
-    } catch (error) {
-        throw new Error(`No se pudo obtener la información de la api: ${error.message}`)
-    }
-    
-}
 
-module.exports = gedRecipeData;
+
+
