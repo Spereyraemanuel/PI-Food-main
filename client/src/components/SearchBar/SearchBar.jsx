6bb5a06085ba5405373d9922eaa72566a8b13fa9
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-05-04 18:29:55 -0300

    actualizado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,38 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
-import style from "./SearchBar.module.css"
+import { getRecipes, getQueryRecipe } from "../../redux/actions";
+import style from "./SearchBar.module.css";
+
+export default function Searchbar(props) {
+    const [input, setInput] = useState("");
+    const dispatch = useDispatch()
+
+
+    const searchHandler = (event) => {
+        const { value } = event.target
+        if (value) {
+            dispatch(getQueryRecipe(value))
+        } else {
+            dispatch(getRecipes())
+        }
+    }
+}
+const handlerInput = (event) => {
+    if (!event.target.value) {
+        dispatch(getRecipes());
+        setInput('')
+    } else {
+        setInput(event.target.value)
+    }
+}
+const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+        searchHandler(event)
+    }
+}
+return (
+ <div>
+    <input type="text" name="search" placeholder="Recipe" value={input} onChange={handlerInput} onKeyDown={handleKeyPress}/>
+    <button onClick={searchHandler} value={input}>Search</button>
+ </div>
+)
