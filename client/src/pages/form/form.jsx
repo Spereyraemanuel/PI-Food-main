e779b7f401d17970c7d9210fd60a851d95bdfb70
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-05-04 21:32:25 -0300

    actualizado

--- client/src/pages/form/form.jsx
+++ client/src/pages/form/form.jsx
@@ -1,35 +1,9 @@
 
 const Form = () => {
     return (
-        <Form>
-            <div>
-                <label htmlFor="">Name</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="">Summary</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="">HealtScore</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="">Steps</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="">Image</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="">diets</label>
-                <input type="text" />
-            </div>
-            <div>
-                <button>Create recipe</button>
-            </div>
-        </Form>
+        <div>
+            <h1>hola mundo</h1>
+        </div>
     )
 }
 
