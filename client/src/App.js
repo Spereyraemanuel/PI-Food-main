2dc56251b3ae33bf6f087f692c79f2ad2af575e1
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-05-06 07:07:30 -0300

    actualizado

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home, Detail, Form, Landing } from "./pages"
 import NavBar from './components/NavBar/NavBar';
@@ -12,22 +12,13 @@ function App() {
 
     <div className="App">
       {location.pathname !== "/" && <NavBar />}
-      <Route path="/home">
-        <Home></Home>
-      </Route>
-
-      <Route path="/create">
-        <Form></Form>
-      </Route>
-
-      <Route path="/detail">
-        <Detail></Detail>
-      </Route>
-
-      <Route exact path="/">
-        <Landing></Landing>
-      </Route>
-
+      <Routes>
+        <Route exact path="/" element={<Landing />} />;
+        <Route path="/home" element={<Home />} />;
+        <Route path="/detail/:idRecipe" element={<Detail />} />;
+        <Route path="/create" element={<Form />} />;
+      </Routes>
+     
     </div>
   );
 }
