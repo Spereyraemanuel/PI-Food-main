4012ec0fc28b75db189f1c2f0c5885331ff0963f
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-05-07 17:55:15 -0300

    actualizado

--- client/src/components/RecipesCointeiner/RecipesContainer.jsx
+++ client/src/components/RecipesCointeiner/RecipesContainer.jsx
@@ -1,51 +1,27 @@
-import Paginado from "../Paginado/Paginado"
 import Recipes from "../Recipes/Recipes"
 import style from "./RecipesContainer.module.css"
-import { useSelector } from "react-redux"
+import { useSelector }  from "react-redux"
 
-export default function RecipesContainter() {
+const RecipesContainter = () => {
 
-  const infoRecipes = useSelector(state => state.recipes)
-  const finalPage = page * 9;
-  const startPage = finalPage - 9;
-  const actualPage = Recipes?.slice(startPage, finalPage)
-  const totalPage = Math.ceil(Recipes.length / 9);
-  const handlerPrevPage = () => {
-    setPage(page - 1);
-  };
-  const handlerNextPage = () => {
-    setPage(page + 1);
-  };
-  const handlerPageNumber = (n) => {
-    setPage(n);
-  };
-
-
-
-  return (
-    <div className={style.container}>
-      {infoRecipes.map(recipe => {
-        return (
-          <div>
-              <Paginado
-                totalPages={totalPage}
-                page={page}
-                prevPage={handlerPrevPage}
-                nextPage={handlerNextPage}
-                pageNumber={handlerPageNumber}
-              />
-              <Recipes
-                key={recipe.id}
-                name={recipe.name}
-                summary={recipe.summary}
-                image={recipe.image}
-                healthScore={recipe.healthScore}
-                diets={recipe.diets}
-              />
-            </div>
-        )
-      })}
-    </div>
-  )
+    const infoRecipes = useSelector(state=>state.recipes)
+     console.log(infoRecipes)
+    return(
+        <div className={style.container}>
+          {infoRecipes.map(recipe=>{
+            return <Recipes
+             key={recipe.id}
+               name={recipe.name}
+               summary={recipe.summary}
+               image={recipe.image}
+               healthScore={recipe.healthScore}
+               diets={recipe.diets}
+            />
+          })}
+          
+             
+        </div>
+    )
 }
 
+export default RecipesContainter
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,15 +1,59 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getRecipes } from "../../redux/actions";
 import RecipesContainter from "../../components/RecipesCointeiner/RecipesContainer";
+import {
+getRecipes,
+filterbyOrigin,
+filterRecipeByDiets,
+orderRecipeAlphabetic,
+healthScoreOrder,
+getDiets,
+deleteFilter,
+cleanStates,
+} from "../../redux/actions"
 
 
 
 export default function Home() {
+ const {myRecipes, loading, diets} = useSelector((state) => state);
+
+
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getRecipes());
-    }, [dispatch])
+        dispatch(cleanStates())
+    }, [dispatch]);
+
+    useEffect(()=> {
+   if(!diets.length){
+    dispatch(getDiets())
+   }
+})
+
+const filterHandler = (event) => {
+    const {name, value } = event.target;
+    if(name === "Diets") {
+        dispatch(filterRecipeByDiets(value));
+    }else {
+        dispatch(filterbyOrigin(value));
+    }
+    if(value === "All"){
+        dispatch(deleteFilter())
+    }
+};
+
+const orderHandler = (event) => {
+    const {name, value} = event.target;
+    if(name === "Alphabetic"){
+        dispatch(orderRecipeAlphabetic(value));
+    }else {
+        dispatch(healthScoreOrder(value));
+    }
+};
+const reset = () => {
+    dispatch(deleteFilter())
+}
 
     return (
         <div >
