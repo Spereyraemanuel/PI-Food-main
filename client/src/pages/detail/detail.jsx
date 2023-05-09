24075e8fa851e912b12aaecb1f730cdd15a319d4
spereyraemanuel <xXmanu222Xx@gmail.com>
2023-05-08 22:18:08 -0300

    actualizado

--- client/src/pages/detail/detail.jsx
+++ client/src/pages/detail/detail.jsx
@@ -1,8 +1,36 @@
- 
-export default function Detail(){
-    return(
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getDetail } from "../../redux/actions"
+import { useParams } from "react-router-dom";
+
+export default function Detail() {
+
+    const { id } = useParams()
+    const { detail } = useSelector((state) => state)
+    const regExp = /<[^>]*>/g;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (id) {
+            dispatch(getDetail(id))
+        }
+    }, [dispatch, id]);
+
+    return (
         <div>
-            <h1>esto es Detail</h1>
+            <h2>{detail.name}</h2>
+            <img src={detail.image} alt="" />
+            <h3>HealtScore: {detail.healthScore} </h3>
+            <h4>Summary: </h4>
+            <p>{detail.summary?.replace(regExp, "")}</p>
+            {detail.steps?.map((s) => {
+                return (
+                    <div key={s.number}>
+                     <p>paso: {s.number}</p>
+                     <p>{s.step}</p>
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
